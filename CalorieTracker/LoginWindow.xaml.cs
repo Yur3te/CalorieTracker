de08be45915b617ac5262f8a4ee7d21f1e0e6585
Yur3te <yur3te@gmail.com>
2026-03-30 19:46:18 +0200

    base project commit

---
+++ CalorieTracker/LoginWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CalorieTracker
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string LoginGiven = LoginTextBox.Text;
+            string PasswordGiven = PasswordTextBox.Password;
+
+            using (var db = new CalorieTrackerDBEntities())
+            {
+                var user = db.Users.FirstOrDefault(u => u.Username == LoginGiven && u.Password == PasswordGiven);
+
+                if (user != null)
+                {
+                    MainWindow main = new MainWindow();
+                    main.Show();
+
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong login or password, how did you forget already", "Error");
+                }
+            }
+        }
+    }
+}
---
+++ CalorieTracker/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalorieTracker
+{
+    public partial class MainWindow : Window
+    {
+        private int LoggedUserId = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (var db = new CalorieTrackerDBEntities())
+            {
+                ProductsComboBox.ItemsSource = db.Products.ToList();
+
+                var dataToTable = db.MealItems
+                    .Where(mi => mi.Meal.UserId == LoggedUserId)
+                    .OrderByDescending(mi => mi.Meal.Date)
+                    .Select(mi => new
+                    {
+                        Data = mi.Meal.Date,
+                        Type = mi.Meal.MealType,
+                        Product = mi.Product.Name,
+                        Weight = mi.WeightInGrams,
+                        CaloriesCalculated = (mi.WeightInGrams * mi.Product.CaloriesPer100g) / 100
+                    })
+                    .ToList();
+
+                TabelaPosilkow.ItemsSource = dataToTable;
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new CalorieTrackerDBEntities())
+            {
+                Meal NewMeal = new Meal();
+                NewMeal.UserId = LoggedUserId;
+                NewMeal.MealType = MealTypeTextBox.Text;
+                NewMeal.Date = System.DateTime.Now;
+
+                db.Meals.Add(NewMeal);
+                db.SaveChanges();
+
+                MealItem MealDetail = new MealItem();
+                MealDetail.MealId = NewMeal.Id; 
+                MealDetail.ProductId = (int)ProductsComboBox.SelectedValue;
+                MealDetail.WeightInGrams = int.Parse(WeightTextBox.Text);
+
+                db.MealItems.Add(MealDetail);
+                db.SaveChanges();
+
+                MessageBox.Show("Meal Added!");
+                LoadData();
+            }
+        }
+    }
+}
